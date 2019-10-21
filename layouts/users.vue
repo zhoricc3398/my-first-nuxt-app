4127c294a9f8a9a9c8424a2be2013f216c35995b
g.sharabidze <g.sharabidze@vendoo.ge>
2019-10-21 14:00:51 +0400

    feat: 18
    Adding a New Layout

---
+++ layouts/users.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <header>
+      USERS SECTION
+    </header>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "users.vue"
+    }
+</script>
--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -6,6 +6,7 @@
     export default {
         validate(data) {
             return /^\d+$/.test(data.params.id)
-        }
+        },
+        layout: 'users'
     }
 </script>
